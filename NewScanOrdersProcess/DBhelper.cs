148d7664e6b513ae63c0cd7cb0e16174c851b39f
Ryan Freemark <freemark21@gmail.com>
2021-02-08 16:03:13 -0600

    Update DBhelper.cs

--- NewScanOrdersProcess/DBhelper.cs
+++ NewScanOrdersProcess/DBhelper.cs
@@ -4,7 +4,7 @@
     {
         public static string CnnVal(string name)
         {
-            return "Server=REP-WWW; Database=NewScanOrders; User Id=Scanorders; Password=Distribution!;";
+            return "Server=REP-WWW; Database=NewScanOrders; User Id=; Password=;";
         }
 
     }
